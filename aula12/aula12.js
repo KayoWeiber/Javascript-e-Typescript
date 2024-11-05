62ae8c82183928709c2f18acb9c835931a143338
Kayo Weiber <caioveiber598@gmail.com>
2024-11-05 14:48:48 -0300

    Create aula12.js

---
+++ aula12/aula12.js
@@ -0,0 +1,13 @@
+let varA="A" //B
+let varB="B" //C
+let varC="C" //A
+
+/*
+const varAtenp=varA
+varA=varB
+varB=varC
+varC=varAtenp
+*/
+[varA,varB,varC]=[varB,varC,varA]
+
+console.log(varA,varB,varC)

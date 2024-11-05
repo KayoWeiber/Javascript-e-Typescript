b43985e18d30994e0526f339a7da1102c4e8a101
Kayo Weiber <caioveiber598@gmail.com>
2024-11-05 10:22:17 -0300

    att

--- aula11/index.js
+++ aula11/index.js
@@ -4,6 +4,40 @@
     ** potenciação
     % resto da divisão
 
-
+-Ordem
+() - 1º
+** - 2º
+* / % - 3º
++ -
 
 */
+/*
+let contador =1
+contador++ //incrementa o valor da váriavel a si mesma
+++contador //incrementa o valor da váriavel a si mesma porém realiza a soma primeiro
+console.log(++contador)
+--contador
+--contador
+console.log(--contador)
+*/
+
+/*
+const passo=2
+let contador=0
+ 
+contador=contador+passo
+console.log(contador)
+contador=contador+passo
+console.log(contador)
+contador+=passo
+console.log(contador)
+
+
+//NaN - Not a Number
+*/
+const num1=10
+//const num2=parseInt("5") //converter a string para inteiro
+//const num2=parseFloat("5.7")
+const num2=Number("5")
+console.log(num1+num2)
+console.log(typeof num2)

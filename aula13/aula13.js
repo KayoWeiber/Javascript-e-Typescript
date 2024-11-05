4f1ec137d2bb46b8efb757504f326cd0b4a39f10
Kayo Weiber <caioveiber598@gmail.com>
2024-11-05 15:20:22 -0300

    att

---
+++ aula13/aula13.js
@@ -0,0 +1,12 @@
+//string = texto
+
+let umastring="o rato roeu a roupa do rei de roma"
+//console.log(umastring[18])
+//console.log(umastring.charAt(4))
+//console.log(umastring.indexOf("o")) //achar a posição
+//console.log(umastring.lastIndexOf("o")) //achar a posição de trás para frente
+//console.log(umastring.match(/[a-z]/g)) //procrura letras minusculas
+//console.log(umastring.replace(/r/g,"#")) //troca uma palavra por outra
+//console.log(umastring.slice(2,7)) //recortar
+//console.log(umastring.slice(2,7)) 
+console.log(umastring.split(" ")) //para separar o texto por espaço coloca em um array

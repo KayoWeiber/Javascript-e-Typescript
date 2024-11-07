b24efb6e6c173702699159fb4eb47530e07b4e96
Kayo Weiber <caioveiber598@gmail.com>
2024-11-07 10:35:09 -0300

    att

---
+++ aula17/index.js
@@ -0,0 +1,23 @@
+/*function saudacao(nome){
+    //console.log(`Olá ${nome}`)
+    return `Olá ${nome}`
+}
+const variavel=saudacao("Luiz")
+console.log(variavel)*/
+/*
+function soma(x,y){
+    const resultado= x+y
+    return resultado
+}*/
+/*
+const raiz=function(n){
+    return n**0.5
+}
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25)) */
+const raiz= n =>  n**0.5; //allow function
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))

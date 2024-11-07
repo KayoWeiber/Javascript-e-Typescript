d6420ee6ae786595864a668fb0f2f3bcc320f573
Kayo Weiber <caioveiber598@gmail.com>
2024-11-07 10:19:40 -0300

    att

---
+++ aula16/aula16.js
@@ -0,0 +1,25 @@
+const alunos= ["Luiz", "João", "Maria"]
+
+alunos.push("Luiza")
+alunos.push("Eduardo")
+//delete alunos[1] // deixa um buraco no awaray
+//console.log(alunos.slice(0,3)) //fatiar o array
+//console.log(alunos.slice(0,-2))
+//console.log(alunos instanceof Array) // para saber se é um array
+
+//const removido=alunos.pop() // remove o ultimo
+//const removido=alunos.shift() // remove o começo
+//alunos.push("Otavio")// coloca no final
+//alunos.push("Luiza")
+//alunos.unshift("Luiza") // acrescenta no começo
+//alunos.unshift("Fábio")
+
+//alunos[0]="Kayo" //altera
+//alunos[alunos.length]="Luiza"
+//alunos[alunos.length]="Fábio"
+//alunos[alunos.length]="Luana"
+
+//console.log(alunos.length) // para saber o tamanho do array
+
+
+//console.log(alunos[2])

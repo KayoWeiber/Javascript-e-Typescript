bd6d102256e7fb8e4f534dd133868436a1b153d7
Kayo Weiber <caioveiber598@gmail.com>
2024-11-13 10:59:24 -0300

    aula 34

---
+++ aula34/index.js
@@ -0,0 +1,17 @@
+/*for (let i=-10;i<=10;i+=2){
+    console.log(`Linha ${i}`)
+}*/
+
+/*
+for(let i=0;i<=50;i++){
+    if (i%2===0){
+        console.log(`${i} é par`)
+    }
+   const par=i%2===0 ?"par":"impar"
+   console.log(`${i} é ${par} `)
+}
+*/
+const array=["Maça","Banana","Pera","Abacate","Uva"]
+for (let i=0;i<array.length;i++){
+    console.log(`Indice ${i}`,array[i])
+}

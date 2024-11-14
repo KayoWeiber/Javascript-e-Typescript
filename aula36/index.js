a83751e810d573da81ea3fcc86e048e910197ec7
Kayo Weiber <caioveiber598@gmail.com>
2024-11-14 11:53:37 -0300

    aula 36

---
+++ aula36/index.js
@@ -0,0 +1,30 @@
+//const frutas=["Pera","maça","Uva"]
+const pessoas={
+    nome:"Luiz",
+    sobrenome:"Otavio",
+    idade:30
+}
+for(let chave in pessoas){
+    console.log(chave,":",pessoas[chave])
+}
+
+
+
+/*
+for (let i in pessoas){
+    console.log(i)
+}
+*/
+
+/*
+for(let i in frutas){
+    console.log(frutas[i])
+}
+
+*/
+
+/*
+for (let i =0;i<frutas.length;i++){
+    console.log(frutas[i])
+} 
+*/

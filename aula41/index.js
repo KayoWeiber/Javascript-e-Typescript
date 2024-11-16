e63f9ad76449bbeeae97b13fc95852915d8c118d
Kayo Weiber <caioveiber598@gmail.com>
2024-11-16 10:57:16 -0300

    aula 40 e 41

---
+++ aula41/index.js
@@ -0,0 +1,10 @@
+/*
+function maiorNum(num1,num2){
+    let maior=Math.max(num1,num2)
+    return maior
+}
+let numeros=maiorNum(54,12)
+console.log(numeros)
+*/
+const max2=(x,y)=> x > y ? x : y
+console.log(max2(1000,50))
